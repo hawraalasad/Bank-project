16845c2aeb5237ca85bb860f658680bb08ff9de5
hawraalasad <hawraalasad@yahoo.com>
2024-09-03 20:01:01 +0300

    i Hate coded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import gringotts from "../src/assets/media/Untitled design (11).png";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+
+  );
 }
 
 export default App;
