df61c63eb717124c3d6925b7be39add99b52fa88
amnaalrashid <amna.alrashid@gmail.com>
2024-09-04 17:26:49 +0300

    k

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
       <div className="max-w-3xl text-center">
-        <h1 className="text-6xl font-bold mb-8">Unleash Your Creativity</h1>
+        <h1 className="text-6xl font-bold mb-8">Welcome to our Bank</h1>
         <p className="text-2xl leading-relaxed text-left">HIIII</p>
       </div>
     </div>
