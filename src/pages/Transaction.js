00b6aab4a604fe5eda9e71cb747e06df39d48307
hawraalasad <hawraalasad@yahoo.com>
2024-09-09 20:08:49 +0300

    fixed date

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -8,48 +8,26 @@ import WithdrawModal from "./WithdrawModal";
 import Filter from "./Filter";
 
 const Transaction = () => {
-  const [query, setQuery] = useState("");
   const [type, setType] = useState("");
-  const [search, setSearch] = useState("");
+  const [amount, setAmount] = useState([]);
+  const [date, setDate] = useState("");
   const { data: myself } = useQuery({
     queryKey: ["me"],
     queryFn: me,
   });
   console.log(myself);
 
-  const [filtered, setFiltered] = useState([]);
-  const [amount, setAmount] = useState([]);
-  const [date, setDate] = useState("");
-
   const handleType = (event) => {
     let selectedType = event.target.value;
     setType(selectedType);
-    const transactionType = transaction?.filter(
-      (transaction) => selectedType === "" || transaction.type === selectedType
-    );
-
-    setFiltered(transactionType);
   };
 
   const handleAmount = (event) => {
     const selectedAmount = event.target.value;
     setAmount(selectedAmount);
-    const transactionAmount = transaction?.filter(
-      (transaction) =>
-        selectedAmount === "" ||
-        transaction.amount.toLocaleString().includes(selectedAmount)
-    );
-    setFiltered(transactionAmount);
   };
 
-  const handleDate = () => {
-    const transactionDate = transaction?.filter(
-      (transaction) =>
-        new Date(date.from) <= new Date(transaction.createdAt) &&
-        new Date(date.to) >= new Date(transaction.createdAt)
-    );
-    setFiltered(transactionDate);
-  };
+  const handleDate = () => {};
 
   const { data: transaction } = useQuery({
     queryKey: ["transactions"],
@@ -57,9 +35,22 @@ const Transaction = () => {
   });
   console.log(transaction);
 
-  const filter = transaction?.filter((trans) => {
-    return trans.type.includes(type);
-  });
+  const filter = transaction
+    ?.filter((trans) => {
+      return trans.type.includes(type);
+    })
+    .filter((transaction) => {
+      return transaction.amount.toString().includes(amount);
+    })
+    .filter((transaction) => {
+      return (
+        date == "" ||
+        (new Date(transaction.createdAt).toLocaleDateString("en-US") >=
+          new Date(date.from).toLocaleDateString("en-US") &&
+          new Date(transaction.createdAt).toLocaleDateString("en-US") <=
+            new Date(date.to).toLocaleDateString("en-US"))
+      );
+    });
 
   return (
     <div
@@ -163,7 +154,14 @@ const Transaction = () => {
                       </span>
                     </td>
                     <td className="w-1/3 text-center">
-                      {new Date(transaction?.createdAt).toLocaleDateString()}
+                      {new Date(transaction?.createdAt)
+                        .toLocaleDateString("en-US")
+                        .slice(
+                          0,
+                          new Date(transaction?.createdAt)
+                            .toLocaleDateString("en-US")
+                            .indexOf(",")
+                        )}
                     </td>
                     <td className="w-1/3 text-center">
                       {/* Conditional styling for the type red for withdraw and tranfer, green for deposit */}
